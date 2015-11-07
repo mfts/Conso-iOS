7bdc88957514ff2a58c19c15c68b7ef64dd678f5
Marc Ft <marcftone@gmail.com>
2015-11-07 14:24:20 +0100

    Initial Init

---
+++ Conso-iOSUITests/Conso_iOSUITests.swift
@@ -0,0 +1,36 @@
+//
+//  Conso_iOSUITests.swift
+//  Conso-iOSUITests
+//
+//  Created by Marc Ft on 07/11/15.
+//  Copyright © 2015 Quantum. All rights reserved.
+//
+
+import XCTest
+
+class Conso_iOSUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
